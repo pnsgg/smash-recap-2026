3e1cd67d493a7fd0e4d90a62f0106f39e028beb2
Gérald Leban <gerald@kilya.net>
2026-08-03 11:39:38 +0200

    test(player): add dayOfWeekActivity tests for player

--- tests/domain/recap/player/player.test.ts
+++ tests/domain/recap/player/player.test.ts
@@ -1,4 +1,6 @@
-import { describe, test } from 'vitest'
+import { PlayerFactory } from '#tests/factories/player-factory.ts'
+import { TournamentFactory } from '#tests/factories/tournament-factory.ts';
+import { describe, expect, test } from 'vitest'
 
 describe('Player', () => {
   test.todo('mostPlayedCharacters')
@@ -8,4 +10,46 @@ describe('Player', () => {
   test.todo('totalSets')
   test.todo('worstMatchups')
   test.todo('uniqueOpponentsFaced')
-})
+
+  describe("dayOfWeekActivity", () => {
+    test("should not contains values if the player did not attend any tournaments this year", () => {
+      const player = PlayerFactory.make();
+
+      expect(player.dayOfWeekActivity()).toEqual([
+        { count: 0, day: "Sun", },
+        { count: 0, day: "Mon", },
+        { count: 0, day: "Tue", },
+        { count: 0, day: "Wed", },
+        { count: 0, day: "Thu", },
+        { count: 0, day: "Fri", },
+        { count: 0, day: "Sat", },
+      ]);
+    });
+
+
+    test("should contains values if the player did attend tournaments this year", () => {
+      const player = PlayerFactory.merge({
+        tournaments: [
+          ...TournamentFactory.merge({ startDate: new Date("2026-07-25") }).makeMany(100),
+          ...TournamentFactory.merge({ startDate: new Date("2026-07-26") }).makeMany(1),
+          ...TournamentFactory.merge({ startDate: new Date("2026-07-28") }).makeMany(5),
+          ...TournamentFactory.merge({ startDate: new Date("2026-07-29") }).makeMany(3),
+          ...TournamentFactory.merge({ startDate: new Date("2026-07-30") }).makeMany(2),
+          ...TournamentFactory.merge({ startDate: new Date("2026-07-31") }).makeMany(9),
+          ...TournamentFactory.merge({ startDate: new Date("2026-08-03") }).makeMany(6),
+
+        ]
+      }).make();
+
+      expect(player.dayOfWeekActivity()).toEqual([
+        { count: 1, day: "Sun", },
+        { count: 6, day: "Mon", },
+        { count: 5, day: "Tue", },
+        { count: 3, day: "Wed", },
+        { count: 2, day: "Thu", },
+        { count: 9, day: "Fri", },
+        { count: 100, day: "Sat", },
+      ]);
+    });
+  });
+});
