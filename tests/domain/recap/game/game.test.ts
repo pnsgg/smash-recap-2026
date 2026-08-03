362bb4d209bc1f7c8805c0d866bd8abf18832e08
Gérald Leban <gerald.leban0@gmail.com>
2026-08-03 21:12:49 +0200

    test: add unit tests for game getPlayerCharacter

--- tests/domain/recap/game/game.test.ts
+++ tests/domain/recap/game/game.test.ts
@@ -1,5 +1,30 @@
-import { describe, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
+import { GameFactory } from '#tests/factories/game-factory'
+import { CharacterFactory } from '#tests/factories/character-factory'
+import { GameSelection } from '#/domain/recap/game'
+import { asPlayerId } from '#/domain/shared-kernel/ids'
 
 describe('Game', () => {
   test.todo('initialization')
+
+  describe('getPlayerCharacter', () => {
+    test('returns character if player has a selection', () => {
+      const playerId = asPlayerId('1')
+      const character = CharacterFactory.merge({ name: 'Fox' }).make()
+      const game = GameFactory.merge({
+        selections: [new GameSelection(playerId, character)],
+      }).make()
+
+      expect(game.getPlayerCharacter(playerId)).toBe(character)
+    })
+
+    test('returns null if player does not have a selection', () => {
+      const playerId = asPlayerId('1')
+      const game = GameFactory.merge({
+        selections: [],
+      }).make()
+
+      expect(game.getPlayerCharacter(playerId)).toBeNull()
+    })
+  })
 })
